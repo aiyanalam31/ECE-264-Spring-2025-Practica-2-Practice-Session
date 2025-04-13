32839115ac5883c78f88644e93dc6453ea4eefcf
aiyanalam31 <alam31@purdue.edu>
2025-04-13 16:57:51 -0400

    Reverse

--- Problem-1/reverse.c
+++ Problem-1/reverse.c
@@ -18,14 +18,4 @@
 #include <string.h>
 #include "reverse.h"
 
-Node* reverse_list(Node* head) {
-    if (head == NULL || head->next == NULL) {
-        return head;
-    }
-
-    Node* new_head = reverse_list(head->next);
-    head->next->next = head;
-    head->next = NULL;
-
-    return new_head;
-}
+Node* reverse_list(Node* head) {}
